5c1c0020f15d1d27eef98975211e31adfb06cd45
Sai Kiran Anugurthi <saikiran.anugurthi@verizonconnect.com>
2021-03-03 20:58:29 +0000

    added changes

---
+++ CI/test.ps1
@@ -0,0 +1,15 @@
+
+$sqlFiles=(Get-ChildItem /usr/bin/git/TestSQL -Recurse).FullName
+
+# Perform iteration to create the same file in each folder
+foreach ($sqlFile in $Thisfile) {
+    
+    $userdata = Get-Content -Path $sqlFile
+    $userdetails =$userdata.Split(" ")
+
+    #Invoke-Sqlcmd -InputFile "C:\ScriptFolder\TestSqlCmd.sql" | Out-File -FilePath "C:\ScriptFolder\TestSqlCmd.rpt" Output sent to TestSqlCmd.rpt.
+
+    Write-Host "UserName" $userdetails[0]
+    Write-Host "password" $userdetails[1]
+    Write-Host "numberofservers" $userdetails[2]
+}
