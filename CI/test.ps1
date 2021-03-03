9d043e4aac54aa6a69842501ec51259f0738efe4
sak1477 <saikiran.anugurthi@gmail.com>
2021-03-03 21:50:40 +0000

    sdds

--- CI/test.ps1
+++ CI/test.ps1
@@ -1,5 +1,5 @@
 
-$sqlFiles=(Get-ChildItem TestSQL -Recurse).FullName
+$sqlFiles=(Get-ChildItem savdssdf -Recurse).FullName
 
 # Perform iteration to create the same file in each folder
 foreach ($sqlFile in $Thisfile) {
